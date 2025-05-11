9ed6ae522065f5d77244075564b9ef5d5783f370
mahmood1992altaie <68857956+mahmood1992altaie@users.noreply.github.com>
2025-05-11 05:47:23 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/supp')
+def supp():
+    return render_template('supp.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+    
+    
+    
---
+++ requirement.ccp
@@ -0,0 +1,8 @@
+project/
+│
+├── app.py
+├── static/
+│   └── img/
+│       └── my_image.jpg
+└── templates/
+    └── home.html
